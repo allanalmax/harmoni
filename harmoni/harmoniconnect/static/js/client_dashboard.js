d45dd5590e9d81be2c7a40954ee4ac101b97817c
Cynthia Ankunda <cynthiaankunda@yahoo.com>
2024-06-18 05:44:09 +0300

    Add files via upload
    client dashboard logic added

---
+++ harmoni/harmoniconnect/static/js/client_dashboard.js
@@ -0,0 +1,16 @@
+
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.className += " active";
+}
